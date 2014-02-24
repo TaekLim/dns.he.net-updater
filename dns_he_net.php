4b2f4657c0bdef3e611154b40464125e2d422a7c
Taek Lim <imteddy@gmail.com>
2014-02-24 23:59:08 +1100

    Make no display curl result when accessing he.net

--- dns_he_net.php
+++ dns_he_net.php
@@ -31,38 +31,34 @@ if (file_exists($last_ip_file)){
 	if ($debug) echo "Last IP was " . $last_ip . "\n";
 }
 
-
 // Get Current IP
-if (exec('curl ifconfig.me > '.$last_ip_file.' 2>&1'))
+exec('curl ifconfig.me > '.$last_ip_file.' 2>&1');
+
+// Check file exist
+if (!file_exists($last_ip_file) && $debug)
+	echo "File not exist!\n";
+
+// Read new ip address
+$ip_add_new = file($last_ip_file);
+$new_ip = trim(end($ip_add_new));
+
+// Debug
+if ($debug) echo "New IP is " . $new_ip . "\n";
+
+if ($new_ip != $last_ip || !file_exists($last_ip_file))
 {
-	// Check file exist
-	if (!file_exists($last_ip_file))
+	if ($debug) echo 'http://'.$domain_name.':'.$ddns_key.'@dyn.dns.he.net/nic/update?hostname='.$host_name.'\n';
+	
+	exec('curl -4 "http://'.$domain_name.':'.$ddns_key.'@dyn.dns.he.net/nic/update?hostname='.$host_name.'" > /dev/null 2>&1');
+
+	// Send email
+	if ($send_email)
 	{
-		if ($debug) echo "File not exist!\n";
+		exec('echo "'.$new_ip.'" | mail -s "'.$email_title.'" '.$email_to);
 
-		exit();
-	}
-
-	$ip_add_new = file($last_ip_file);
-
-	// Debug
-	if ($debug) echo "New IP is " . end($ip_add_new) . "\n";
-
-	if (end($ip_add_new) != $last_ip || !file_exists($last_ip_file))
-	{
-		$result = array();
-		exec('curl -4 "http://'.$domain_name.':'.$ddns_key.'@dyn.dns.he.net/nic/update?hostname='.$host_name.'"', $result);
-
-		// Send email
-		if ($send_email)
-		{
-			exec('echo "'.end($ip_add_new).'" | mail -s "'.$email_title.'" '.$email_to);
-
-			// Debug
-			if ($debug) echo "Email sent\n";
-		}
-			
 		// Debug
-		if ($debug) print_r($result);
+		if ($debug) echo "Email sent\n";
 	}
+}else if ($debug) {
+	echo "Still in same IP.\n";
 }
